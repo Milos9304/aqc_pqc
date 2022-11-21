1e67d1b4a25aeec45773ea393ed4e5ca9028e069
Your Name <you@example.com>
2022-11-21 13:00:35 +0000

    plotting high energy states

--- plot/plot_diff.py
+++ plot/plot_diff.py
@@ -11,12 +11,20 @@ for file_name in glob.glob("*.log"):
     y=[]
     evals = []
     expecs = []
+    hes = {}
+    i=0
     for line in f.readlines():
         #print(line)
-        eval, expec = line[:-1].split(' ')[:2]
-        evals.append(float(eval))
-        expecs.append(float(expec))
-        #y.append(abs(float(eval)-float(expec)))
+        
+        if line.startswith("HES: "):
+            hes[i] = list(map(float, line.split(' ')[1:]))
+            i+=1
+        else:
+            eval, expec = line[:-1].split(' ')[:2]
+            evals.append(float(eval))
+            expecs.append(float(expec))
+            #y.append(abs(float(eval)-float(expec)))
+        
     
     x = np.linspace(0, len(evals)-1, len(evals))
     #ys.append(y)
@@ -24,8 +32,30 @@ for file_name in glob.glob("*.log"):
     #for i in range(len(evals)):
     #    print(evals[i],' ',expecs[i],' ',expecs[i]-evals[i])
     #njfkdvkslnjk
-    plt.plot(x, evals)
-    plt.plot(x, expecs)
+    
+    firstHES = []
+    for i in range(len(x)):
+        firstHES.append(hes[i][1])
+    secondHES = []
+    for i in range(len(x)):
+        secondHES.append(hes[i][2])
+    thirdHES = []
+    for i in range(len(x)):
+        thirdHES.append(hes[i][3])
+    fourthHES = []
+    for i in range(len(x)):
+        fourthHES.append(hes[i][4])
+    
+    print(evals)
+    print()
+    print(firstHES)
+    
+    plt.plot(x, evals, 'orange')
+    plt.plot(x, expecs, 'blue')
+    plt.plot(x, firstHES, color=(1, 0, 0))
+    plt.plot(x, secondHES, color=(1, 0.2, 0.2))
+    plt.plot(x, thirdHES, color=(1, 0.4, 0.4))
+    plt.plot(x, fourthHES, color=(1, 0.6, 0.6))
     plt.title(file_name)
     plt.show()
         
@@ -34,11 +64,3 @@ for file_name in glob.glob("*.log"):
     #plt.plot(expecs)
     #plt.show()
     
-
-print(len(ys[0]))
-jk
-for i in range(len(ys)):
-    
-    plt.plot(ys[i]+0.01*i*np.ones((len(ys[i]))), 'o')
-
-plt.show()
