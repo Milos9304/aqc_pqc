d66b8e36e9ebdc12a4429b724de647bf9825405a
Your Name <you@example.com>
2023-04-03 14:10:12 +0100

    adding final params for step experiment

---
+++ step_experiment/save_params.py
@@ -0,0 +1,17 @@
+import glob, os
+import numpy as np
+
+res = []
+for i in range(31):
+    res.append([[0]*20]*20)
+
+for steps in [5,10,15,20,25,30]:
+
+    for seed in range(20):
+        file="step_experiment_"+str(steps)+"/10_"+str(seed)+".bkp"
+        with open(file, 'r') as f:
+            lines = f.read().splitlines()
+            row=list(map(float, lines[-2][7:-1].split(' ')))
+            res[steps][seed]=row
+
+np.save("final_params.npy", res)
