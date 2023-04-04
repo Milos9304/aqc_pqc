14858aac8c9b0842d58b5c47a9cf42f53724cd52
milos_pr <milos_pr@nextgenio-login1.localdomain>
2023-04-04 09:47:21 +0100

    adding step_experiment_35

--- step_experiment/save_params.py
+++ step_experiment/save_params.py
@@ -2,10 +2,10 @@ import glob, os
 import numpy as np
 
 res = []
-for i in range(31):
+for i in range(36):
     res.append([[0]*20]*20)
 
-for steps in [5,10,15,20,25,30]:
+for steps in [5,10,15,20,25,30,35]:
 
     for seed in range(20):
         file="step_experiment_"+str(steps)+"/10_"+str(seed)+".bkp"
